f1c4ea5c6b6b74dcdfee61e41916b878865c32cc
Mateusz Orłowski <mateusz0799@gmail.com>
2021-04-07 08:58:39 +0200

    Added .env file, installed dotenv package

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv").config()
+
+module.exports = {
+  token: process.env.token,
+  prefix: '!',
+  guildRoles: {
+    Cyfryzacja: { id: '808429684148535306' },
+    Współpraca: { id: '808429810958991390' },
+    Rozwój: { id: '808429722198736907' },
+    Projekty: { id: '808429771179687956' },
+    Promocja: { id: '808429898036543488' },
+    Dydaktyka: { id: '808430036545699920' },
+    Sport: { id: '808429960909815849' },
+    Miasteczko: { id: '808429993804693574' },
+  },
+};
